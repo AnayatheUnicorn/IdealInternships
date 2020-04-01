33ef256056cee6a0ea0d19fb47946a850093426c
AnayatheUnicorn <55161138+AnayatheUnicorn@users.noreply.github.com>
2020-03-31 21:15:07 -0400

    //The Student Object File with Data

--- app/src/main/java/com/example/idealinternships/Student.java
+++ app/src/main/java/com/example/idealinternships/Student.java
@@ -1,4 +1,23 @@
 package com.example.idealinternships;
 
 public class Student {
+    //Data
+    private String firstName;
+    private String lastName;
+    private String bio;//not required
+    private int age;
+    private String gender;
+    private String race;
+    private String school;
+    private boolean military;
+    private String field;
+    private String location;//not required
+    private String income;//not required
+    //how do we handle preferred dates? do they need to be their own object? might be smart to have a date class
+    //private Date startPreferredDate;
+    //private Date endPreferredDate;
+    private boolean notifications;
+
+    //Constructor
+    
 }
