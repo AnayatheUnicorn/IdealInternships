ff10e05b7c81daca60f99f629cf5a8797effef5e
baohanngo <bngo@wpi.edu>
2020-04-02 14:09:25 -0400

    added firebase info

--- app/src/main/java/com/example/idealinternships/MainActivity.java
+++ app/src/main/java/com/example/idealinternships/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.idealinternships;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
