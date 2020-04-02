c7a96febd61227a3046dd27211f56f274da2cf2d
AnayatheUnicorn <55161138+AnayatheUnicorn@users.noreply.github.com>
2020-04-02 13:50:56 -0400

    //The Student Object File (complete)

--- app/src/main/java/com/example/idealinternships/Student.java
+++ app/src/main/java/com/example/idealinternships/Student.java
@@ -6,6 +6,8 @@ public class Student {
     //Data
     private String firstName;
     private String lastName;
+    private String studentEmail;
+    private String password;
     private String profileBio;
     private int studentAge;
     private String studentGender;
@@ -20,11 +22,13 @@ public class Student {
     private boolean notifications;
 
     //Constructor
-    public Student(String first, String last, String bio, int age, String gender, String race, String school,
-                   boolean milAssociation, String preferredField, String location, String income, Date startDate,
+    public Student(String first, String last, String email, String pass, String bio, int age, String gender, String race,
+                   String school, boolean milAssociation, String preferredField, String location, String income, Date startDate,
                    Date endDate, boolean push){
         firstName = first;
         lastName = last;
+        studentEmail = email;
+        password = pass;
         profileBio = bio;
         studentAge = age;
         studentGender = gender;
@@ -40,99 +44,132 @@ public class Student {
     }
 
     //Methods
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setProfileBio(String profileBio) {
-        this.profileBio = profileBio;
-    }
-
-    public void setStudentAge(int studentAge) {
-        this.studentAge = studentAge;
-    }
-
-    public void setStudentGender(String studentGender) {
-        this.studentGender = studentGender;
-    }
-
-    public void setStudentRace(String studentRace) {
-        this.studentRace = studentRace;
-    }
-
-    public void setStudentSchool(String studentSchool) {
-        this.studentSchool = studentSchool;
-    }
-
-    public void setMilitary(boolean military) {
-        this.military = military;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
-
-    public void setLocation(String location) {
-        this.studentLocation = location;
-    }
-
-    public void setIncome(String income) {
-        this.income = income;
-    }
-
-    public void setNotifications(boolean notifications) {
-        this.notifications = notifications;
-    }
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getProfileBio() {
         return profileBio;
+    }
+
+    public void setProfileBio(String profileBio) {
+        this.profileBio = profileBio;
     }
 
     public int getStudentAge() {
         return studentAge;
     }
 
+    public void setStudentAge(int studentAge) {
+        this.studentAge = studentAge;
+    }
+
     public String getStudentGender() {
         return studentGender;
+    }
+
+    public void setStudentGender(String studentGender) {
+        this.studentGender = studentGender;
     }
 
     public String getStudentRace() {
         return studentRace;
     }
 
+    public void setStudentRace(String studentRace) {
+        this.studentRace = studentRace;
+    }
+
     public String getStudentSchool() {
         return studentSchool;
+    }
+
+    public void setStudentSchool(String studentSchool) {
+        this.studentSchool = studentSchool;
     }
 
     public boolean isMilitary() {
         return military;
     }
 
+    public void setMilitary(boolean military) {
+        this.military = military;
+    }
+
     public String getField() {
         return field;
     }
 
-    public String getLocation() {
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getStudentLocation() {
         return studentLocation;
+    }
+
+    public void setStudentLocation(String studentLocation) {
+        this.studentLocation = studentLocation;
     }
 
     public String getIncome() {
         return income;
     }
 
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public Date getStartPreferredDate() {
+        return startPreferredDate;
+    }
+
+    public void setStartPreferredDate(Date startPreferredDate) {
+        this.startPreferredDate = startPreferredDate;
+    }
+
+    public Date getEndPreferredDate() {
+        return endPreferredDate;
+    }
+
+    public void setEndPreferredDate(Date endPreferredDate) {
+        this.endPreferredDate = endPreferredDate;
+    }
+
     public boolean isNotifications() {
         return notifications;
+    }
+
+    public void setNotifications(boolean notifications) {
+        this.notifications = notifications;
     }
 }
