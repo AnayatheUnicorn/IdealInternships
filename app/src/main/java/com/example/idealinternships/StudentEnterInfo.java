a84e17b5db6d95b9115eef1513c5ffa8dc50396d
AnayatheUnicorn <55161138+AnayatheUnicorn@users.noreply.github.com>
2020-04-06 13:24:18 -0400

    //XML file for student info with Activity

---
+++ app/src/main/java/com/example/idealinternships/StudentEnterInfo.java
@@ -0,0 +1,14 @@
+package com.example.idealinternships;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentEnterInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_enter_info);
+    }
+}
