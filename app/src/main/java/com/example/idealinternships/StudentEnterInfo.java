39ff9d2f1f9bf82b8dc6b71eb31c1517e47c05be
AnayatheUnicorn <55161138+AnayatheUnicorn@users.noreply.github.com>
2020-04-06 13:37:48 -0400

    //XML file for student info with Activity

--- app/src/main/java/com/example/idealinternships/StudentEnterInfo.java
+++ app/src/main/java/com/example/idealinternships/StudentEnterInfo.java
@@ -3,6 +3,8 @@ package com.example.idealinternships;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StudentEnterInfo extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class StudentEnterInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_enter_info);
+
+        Spinner ageSpinner = findViewById(R.id.ageDropdown);
+
+        ArrayAdapter<String> ageAdapter = new ArrayAdapter<String>(StudentEnterInfo.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ages));
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
     }
 }
