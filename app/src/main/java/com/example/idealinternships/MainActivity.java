3deb01753a911025ad35e0e8d00d4f07d37ae633
mna1024 <mnattiogbe@wpi.edu>
2020-04-09 14:21:45 -0400

    Updated Main Activity

--- app/src/main/java/com/example/idealinternships/MainActivity.java
+++ app/src/main/java/com/example/idealinternships/MainActivity.java
@@ -4,14 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,25 +29,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, StudentEnterInfo.class);
         startActivity(intent);
     }
-    private void testMethod(){
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-        System.out.println("random");
-    }
 
-
-    private void nonsense(){
-        System.out.println("hello world");
-        System.out.println("this is a test");
-
+    public void goToUploadInternshipPage(View v){
+        Intent intent = new Intent(this, UploadInternship.class);
+        startActivity(intent);
     }
 
 
