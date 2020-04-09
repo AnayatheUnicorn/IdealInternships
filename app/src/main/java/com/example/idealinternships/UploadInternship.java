07acc19a1fdf72bd0746ca758f5af87141c4ceaf
mna1024 <mnattiogbe@wpi.edu>
2020-04-09 14:34:48 -0400

    Updated Upload Internship Page

--- app/src/main/java/com/example/idealinternships/UploadInternship.java
+++ app/src/main/java/com/example/idealinternships/UploadInternship.java
@@ -12,17 +12,5 @@ public class UploadInternship extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_internship);
-
-        Spinner ageSpinner = findViewById(R.id.ageDropdown);
-        ArrayAdapter<String> ageAdapter = new ArrayAdapter<String>(UploadInternship.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ages));
-        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ageSpinner.setAdapter(ageAdapter);
-
-        Spinner genderSpinner = findViewById(R.id.genderDropdown);
-        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(UploadInternship.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.gender));
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        genderSpinner.setAdapter(genderAdapter);
     }
 }
